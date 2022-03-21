99e1ea4b8e2188691d012f842ffc2c6032714bd9
romanboyarkin <boyra75.rb@gmail.com>
2022-03-21 09:50:10 +0900

    Step.04-Solution-Back-Stack-Manipulation

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -33,7 +33,8 @@ class GameOverFragment : Fragment() {
                 inflater, R.layout.fragment_game_over, container, false)
         // TODO (05) Add an onClick Handler for the tryAgain button that navigates to the gameFragment
         // using action_gameOverFragment_to_gameFragment
-        binding.tryAgainButton.setOnClickListener {v: View? ->
+        binding.tryAgainButton.setOnClickListener {view: View ->
+            view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
 
         }
         return binding.root
